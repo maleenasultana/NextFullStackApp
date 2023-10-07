a3e822c67c7fa50a9a25ff941b8ea5ea4ef2314c
Maleena <maleena.sohail@gmail.com>
2023-10-07 23:26:54 +0530

    changes made

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,15 +1,18 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const Get = async (request, {params}) => {
+export const GET = async (request, { params }) => {
     try {
         await connectToDB()
 
         const prompt = await Prompt.findById(params.id).populate("creator")
-        if(!prompt) return new Response(JSON.stringify(prompt), {status: 200})
-    } catch (error){
-return new Response("Internal Server Error", {status: 500});
-}
+        if (!prompt) return new Response("Prompt Not Found", { status: 404 });
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+
+    } catch (error) {
+        return new Response("Internal Server Error", { status: 500 });
+    }
 }
 
 export const PATCH = async (request, { params }) => {
--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,28 @@
-import '@styles/globals.css'
-import  Nav from '@components/Nav'
-import  Provider  from '@components/Provider'
+import "@styles/globals.css";
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "nextFullStackapp",
-    description: 'Discover & Share AI Prompts'
-}
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
-  return (
- <html lang ='en'>
+const RootLayout = ({ children }) => (
+  <html lang='en'>
     <body>
-        <Provider>
+      <Provider>
         <div className='main'>
-            <div className='gradient'/>
+          <div className='gradient' />
         </div>
-        <main className='app'>
-            <Nav />
-            {children} 
-        </main>
-        </Provider>
-    </body>
- </html>
-  )
-}
 
-export default RootLayout 
+        <main className='app'>
+          <Nav />
+          {children}
+        </main>
+      </Provider>
+    </body>
+  </html>
+);
+
+export default RootLayout;
